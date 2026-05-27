bf20702820f372d27b6e6e15851a0a1fc0821d0f
jawahirenakash <jawahirenakash@gmail.com>
2026-05-27 21:07:52 +0530

    Final: Tidy project and add remaining files

---
+++ MatchMate/MatchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by user295386 on 5/27/26.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MatchListView()
+        }
+    }
+}
